3a8adefe6d3031bd243b23dc8bbc84137880216d
hepeng <hepeng@ehousechina.com>
2019-11-22 16:09:31 +0800

    x

--- conf/Database.php
+++ conf/Database.php
@@ -8,9 +8,9 @@ return [
     'default'=>[
         'type'        => 'mysql',     // 数据库类型  mysql,sqlserver,oracle,sqlite
         'host'        => 'localhost', // 服务器地址
-        'database'    => 'test',// 数据库名
-        'username'    => 'root',// 用户名
-        'password'    => 'root',// 密码
+        'db'    => 'test',// 数据库名
+        'user'    => 'root',// 用户名
+        'pass'    => 'root',// 密码
         'port'        => '3306',// 端口 默认为3306
     ],
 
